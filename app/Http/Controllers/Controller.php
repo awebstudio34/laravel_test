96f9be984c8aa7944f9ba39f0e3deab4992484a5
Антон Выходцев <vykhodtsevai@gmail.com>
2017-10-23 21:01:29 +0300

    Изменения

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 
-class Controller extends BaseController
+
+class PostController extends Controller
 {
-    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+    public function index()
+    {
+        dd('ПРИВЕТ!!!!');
+    }
 }
